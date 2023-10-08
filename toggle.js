c73ba1694e18470b10ac82d89dad6ffaa41a8390
Apnacollege <ajay91044@gmail.com>
2023-10-08 23:01:28 +0530

    adding html css and js file

---
+++ toggle.js
@@ -0,0 +1,11 @@
+let section=document.querySelector(".section")
+let div=document.querySelector(".div")
+let button=document.querySelector(".button")
+
+
+
+function toggle()
+{
+    section.classList.toggle("section")
+    
+}
